90dd247a2d8c0769aac65bf23dc63945ab593e79
Pratyaksh Tyagi <tyagipratyaksh@gmail.com>
2024-07-26 18:39:43 -0500

    Initial Commit

---
+++ USA/USAApp.swift
@@ -0,0 +1,17 @@
+//
+//  USAApp.swift
+//  USA
+//
+//  Created by Pratyaksh Tyagi on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct USAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
